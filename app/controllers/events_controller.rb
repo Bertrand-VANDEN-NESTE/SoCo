51229b9669f0a7c79509a359a4d83ab2bc20565e
Yanis Laamiri <yajdain@gmail.com>
2023-03-08 11:40:40 +0100

    controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,37 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show, destroy]
   def index
     @events = Event.all
   end
 
+  def show; end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current.user
+    if event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, status: :see_other
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :theme, :date, :location, :description, :capacity, :status, :tricount, :password, :photo)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
