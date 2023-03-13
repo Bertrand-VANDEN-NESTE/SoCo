8b8cc8ce36c122cc5c325af4fde94f9f08f89433
Yanis Laamiri <yajdain@gmail.com>
2023-03-13 10:29:48 +0100

    searchbar

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,9 @@ class Event < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
+  include PgSearch::Model
+  pg_search_scope :search_by_location_and_theme, against: %i[location theme],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
