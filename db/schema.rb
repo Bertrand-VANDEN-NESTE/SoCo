18bb47bd1cc8fb2ff33c5f4e63ef5b1476942929
Yanis Laamiri <yajdain@gmail.com>
2023-03-14 10:48:43 +0100

    search bar +table caztegory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_133750) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_090209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_133750) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_categories_on_event_id"
+  end
+
   create_table "chatrooms", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.datetime "created_at", null: false
@@ -63,7 +71,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_133750) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.string "theme"
     t.date "date"
     t.string "location"
     t.text "description"
@@ -133,6 +140,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_133750) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "events"
   add_foreign_key "chatrooms", "events"
   add_foreign_key "event_ratings", "events"
   add_foreign_key "event_ratings", "users"
