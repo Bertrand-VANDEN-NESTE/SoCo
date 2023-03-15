56ed1cd32fb19ba25191a36c35030e67be8d2047
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2023-03-15 18:49:41 +0100

    probleme

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,4 +1,4 @@
 # config/initializers/default_meta.rb
 
 # Initialise les balises meta par défaut.
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
+# DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
