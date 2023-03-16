4849abf9f0fbc59a83ed937dfb606b3f3bd1e948
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2023-03-16 14:46:49 +0100

    modification sur la chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,8 @@
 class ChatroomsController < ApplicationController
   def show
     @event = Event.find(params[:event_id])
-    @chatroom = Chatroom.find(params[:id])
+    # @chatroom = Chatroom.find(params[:id])
+    @chatroom = @event.chatroom
     @message = Message.new
   end
 end
